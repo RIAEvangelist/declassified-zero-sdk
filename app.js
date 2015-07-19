0e326ebdec39cb41e3e08477413b0dc11cf33c3e
Brandon Nozaki Miller <brandon@diginow.it>
2015-07-19 01:39:11 -0700

    fixed module name issue

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var network= require('./lib/network.js');
 var zero= require('./protocols/2015.js');
-var cmd = require('cmd');
+var cmd = require('node-cmd');
 
 cmd.run('sudo ip link set can0 up type can bitrate 500000');
 
